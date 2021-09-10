2c48302951079c73ccef33676158c78c8c378c15
rkh-leung <leptanian@gmail.com>
2021-09-10 12:41:17 +1200

    add: separate performance check from palindrome solution

--- palindrome.js
+++ palindrome.js
@@ -45,17 +45,8 @@ function palindromeLoop(str) {
     return true
 }
 
-const performance = (fn, str, iteration) => {
-    const data = []
-    for (let i = 0; i < iteration; i++) {
-        const start = process.hrtime.bigint()
-        fn(str)
-        const end = process.hrtime.bigint()
-        data.push(end - start)
-    }
-    const ns = data.reduce((a, v) => (a + v), BigInt(0)) / BigInt(data.length)
-    console.log(`\nPassage is ${longPalindrome.length} chars. \nAverage runtime of ${fn.name} after ${iteration} times is: ${ns} nanoseconds. \nIs the passage a palindrome? ${fn(longPalindrome)}`)
-    return ns
+module.exports = {
+    longPalindrome,
+    palindrome,
+    palindromeLoop
 }
-
-console.log(`\nPalindrome is better than PalindromeLoop by ${performance(palindromeLoop, longPalindrome, 20) - performance(palindrome, longPalindrome, 20)} nanoseconds`)
---
+++ performance.js
@@ -0,0 +1,16 @@
+const {longPalindrome, palindrome, palindromeLoop} = require('./palindrome')
+
+const performance = (fn, str, iteration) => {
+    const data = []
+    for (let i = 0; i < iteration; i++) {
+        const start = process.hrtime.bigint()
+        fn(str)
+        const end = process.hrtime.bigint()
+        data.push(end - start)
+    }
+    const ns = data.reduce((a, v) => (a + v), BigInt(0)) / BigInt(data.length)
+    console.log(`\nPassage is ${longPalindrome.length} chars. \nAverage runtime of ${fn.name} after ${iteration} times is: ${ns} nanoseconds. \nIs the passage a palindrome? ${fn(longPalindrome)}`)
+    return ns
+}
+
+console.log(`\nPalindrome is better than PalindromeLoop by ${performance(palindromeLoop, longPalindrome, 20) - performance(palindrome, longPalindrome, 20)} nanoseconds`)
